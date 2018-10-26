454c6db32faa254f387636a3ed6ea0910365196e
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2018-10-26 18:27:04 +0200

    Do two laplacian steps to avoid copying memory

--- algorithms/smoothing/laplacian.cpp
+++ algorithms/smoothing/laplacian.cpp
@@ -14,20 +14,26 @@ namespace smoothing {
 		new_verts.shrink_to_fit();
 
 		// for each iteration of the algorithm
-		for (size_t it = 0; it < nit; ++it) {
+		size_t it;
+		for (it = 0; it < nit; it += 2) {
 			smoothing_private::apply_local(w, l, m, old_verts, new_verts);
-			if (it < nit - 1) {
-				// copy 'new_verts' to 'old_verts'
-				// only when necessary
-				old_verts.assign(new_verts.begin(), new_verts.end());
-			}
+			smoothing_private::apply_local(w, l, m, new_verts, old_verts);
+		}
+		if (it == n) {
+			// do one last iteration
+			smoothing_private::apply_local(w, l, m, old_verts, new_verts);
 		}
 
 		// Replace the coordinates of the old vertices in the
 		// mesh for the vertices in 'old_verts'. Notice that we
 		// are using 'move' for moving from 'new_verts' to 'old_verts'.
 		// That deletes the contents of 'new_verts'
-		m.set_vertices(new_verts);
+		if (it == n) {
+			m.set_vertices(new_verts);
+		}
+		else {
+			m.set_vertices(old_verts);
+		}
 	}
 
 	void bilaplacian(const smooth_weight& w, float l, size_t nit, TriangleMesh& m) {
@@ -55,7 +61,7 @@ namespace smoothing {
 
 	void TaubinLM(const smooth_weight& w, float l, size_t nit, TriangleMesh& m) {
 		const float Kpb = 0.1;
-		float mu = 1.0f/( Kpb - 1.0f/l);
+		float mu = 1.0f/(Kpb - 1.0f/l);
 
 		std::vector<glm::vec3> old_verts;
 		old_verts = m.get_vertices();
