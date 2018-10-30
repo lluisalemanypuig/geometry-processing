d7586c6702af3c66947529b0e90eb488f83a5c64
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2018-10-30 17:44:54 +0100

    Use a binning strategy to display curvature values - Still not good enough

--- interface/glwidget.cpp
+++ interface/glwidget.cpp
@@ -22,91 +22,152 @@ inline
 void make_colors_rainbow_gradient
 (const vector<float>& values, curv_type ct, vector<vec3>& cols)
 {
-	// maximum value to determine the [min,max]
-	// range of the mean curvature
-	const float phi = 0.5;
-
-	// minimum and maximum values of curvature
+	// find minimum and maximum values of curvature
 	float cm = numeric_limits<float>::max();
 	float cM = -numeric_limits<float>::max();
 	for (size_t i = 0; i < values.size(); ++i) {
 		float v = values[i];
-		if (ct == curv_type::Gauss) {
-			if (v < -2*M_PI or v > 2*M_PI) {
-				// if the curvature is outside [-2pi,2pi]
-				// then discard it. The Gauss curvature
-				// can't be greater than 2pi, though.
-				continue;
-			}
-			cm = std::min(cm, v);
-			cM = std::max(cM, v);
-		}
-		else if (ct == curv_type::Mean) {
-			cm = std::min(cm, v);
-			cM = phi;
-		}
+		cm = std::min(cm, v);
+		cM = std::max(cM, v);
 	}
 	float d = cM - cm;
-	cols = vector<vec3>(values.size());
+
+	cout << "min curvature: " << cm << endl;
+	cout << "max curvature: " << cM << endl;
+
+	// do binning on # of bins as a function
+	// of the difference between the maximum
+	// and minimum values
+
+	int nbins = 0;
+	if (d <= 10.0f) {
+		nbins = 10;
+	}
+	else if (d <= 100.0f) {
+		nbins = 100;
+	}
+	else if (d <= 1000.0f) {
+		nbins = 1000;
+	}
+	else if (d <= 10000.0f) {
+		nbins = 10000;
+	}
+	else if (d <= 100000.0f) {
+		nbins = 100000;
+	}
+	else if (d <= 1000000.0f) {
+		nbins = 1000000;
+	}
+
+	cout << "use " << nbins << " bins" << endl;
+
+	vector<int> bins(nbins + 1, 0);
+	float step = d/nbins;
+
+	cout << "step= " << step << endl;
+
+	// i-th bin comprises the values
+	//
+	//  [ m + i*(M - m)/nbins,
+	//    m + (i + 1)*(M - m)/nbins] =
+	// = [m + i*step, m + (i + 1)*step]
+	//
+
+	// maximum size among the bins,
+	// and the corresponding index
+	int max_size = 0;
+	int max_idx = 0;
+	for (size_t i = 0; i < values.size(); ++i) {
+		int idx = (values[i] - cm)/step;
+		++bins[idx];
+
+		if (max_size > bins[idx]) {
+			max_size = bins[idx];
+			max_idx = idx;
+		}
+	}
+
+	// amount of curvature values covered
+	int count = max_size;
+	int left = max_idx;
+	int right = max_idx;
+
+	do {
+		if (left > 0) {
+			--left;
+			count += bins[left];
+		}
+		if (right < nbins) {
+			++right;
+			count += bins[right];
+		}
+	}
+	while ((100.0f*count)/values.size() < 80.0f and (left > 0 or right < nbins));
+
+	cout << "Find 80% of values between: " << endl;
+	cout << "    left=  " << left << endl;
+	cout << "    right= " << right << endl;
+	cout << "    count= " << count << endl;
+	cout << "        proportion: " << float(count)/values.size() << endl;
+
+	// compute actual minimum and maximum
+	cM = cm + right*step;
+	cm = cm + left*step;
+
+	cout << "new minimum: " << cm << endl;
+	cout << "new maximum: " << cM << endl;
 
 	// ----------------------- //
 	// Colour rainbow gradient //
+	cols = vector<vec3>(values.size());
 	for (size_t i = 0; i < values.size(); ++i) {
 
 		float r, g, b;
-		if (values[i] < cm) {
-			r = g = b = 0.0f;
+		float s = (values[i] - cm)/d;
+		if (s <= 0.0f) {
+			r = 0.0f;
+			g = 0.0f;
+			b = 0.0f;
 		}
-		else if (values[i] >= cM) {
-			r = g = b = 1.0f;
+		else if (s <= 0.2f) {
+			// RED
+			// from 0.0 to 0.2
+			r = 5.0f*s;
+			g = 0.0f;
+			b = 0.0f;
 		}
-		else {
-			float s = (values[i] - cm)/d;
-			if (s <= 0.0f) {
-				r = 0.0f;
-				g = 0.0f;
-				b = 0.0f;
-			}
-			else if (s <= 0.2f) {
-				// RED
-				// from 0.0 to 0.2
-				r = 5.0f*s;
-				g = 0.0f;
-				b = 0.0f;
-			}
-			else if (s <= 0.4f) {
-				// YELLOW
-				// from 0.2 to 0.4
-				r = 1.0f;
-				g = 5.0f*s - 1.0f;
-				b = 0.0f;
-			}
-			else if (s <= 0.6f) {
-				// GREEN
-				// from 0.4 to 0.6
-				r = 5.0f*s - 2.0f;
-				g = 1.0f;
-				b = 0.0f;
-			}
-			else if (s <= 0.8f) {
-				// TURQUOISE
-				// from 0.6 to 0.8
-				r = 0.0f;
-				g = 1.0f;
-				b = 5.0f*s - 3.0f;
-			}
-			else if (s <= 1.0f) {
-				// BLUE
-				// from 0.8 to 1.0
-				r = 0.0f;
-				g = 5.0f*s - 3.0f;
-				b = 1.0f;
-			}
-			else if (1.0f <= s) {
-				r = 0.0f;
-				g = 0.0f;
-				b = 1.0f;
-			}
+		else if (s <= 0.4f) {
+			// YELLOW
+			// from 0.2 to 0.4
+			r = 1.0f;
+			g = 5.0f*s - 1.0f;
+			b = 0.0f;
+		}
+		else if (s <= 0.6f) {
+			// GREEN
+			// from 0.4 to 0.6
+			r = 5.0f*s - 2.0f;
+			g = 1.0f;
+			b = 0.0f;
+		}
+		else if (s <= 0.8f) {
+			// TURQUOISE
+			// from 0.6 to 0.8
+			r = 0.0f;
+			g = 1.0f;
+			b = 5.0f*s - 3.0f;
+		}
+		else if (s <= 1.0f) {
+			// BLUE
+			// from 0.8 to 1.0
+			r = 0.0f;
+			g = 5.0f*s - 3.0f;
+			b = 1.0f;
+		}
+		else if (1.0f <= s) {
+			r = 1.0f;
+			g = 1.0f;
+			b = 1.0f;
 		}
 
 		cols[i] = vec3(r,g,b);
