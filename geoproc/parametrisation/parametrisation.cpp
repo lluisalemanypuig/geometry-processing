269d1e129268fd39bc5a897a4267e1aaf9202991
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2018-11-28 12:32:00 +0100

    First implementation of harmonic map parametrisation

--- geoproc/parametrisation/parametrisation.cpp
+++ geoproc/parametrisation/parametrisation.cpp
@@ -7,9 +7,17 @@ using namespace std;
 // geoproc includes
 #include <geoproc/smoothing/local_private.hpp>
 
+// Eigen includes
+#include <Eigen/Sparse>
+
 namespace geoproc {
 using namespace smoothing;
 namespace parametrisation {
+
+	typedef Eigen::Triplet<float> T;
+	typedef Eigen::MatrixXf Matrixf;
+	typedef Eigen::VectorXf Vectorf;
+	typedef Eigen::SparseMatrix<float> SparseMatrixf;
 
 	bool harmonic_maps
 	(
@@ -88,7 +96,8 @@ namespace parametrisation {
 			}
 		}
 
-		int n_constant = boundary.size();
+		const int n_constant = boundary.size();
+		const int variable = N - n_constant;
 
 		// weights for system's matrix
 		vector<vector<float> > ws(N, vector<float>(N, 0.0f));
@@ -105,7 +114,87 @@ namespace parametrisation {
 			}
 		}
 
+		vector<T> triplet_list;
 
+		/* reserve independent term vector */
+		Eigen::VectorXf bX(variable), bY(variable);
+
+		/* fill independent term vector
+		   and compute triplets for system matrix */
+
+		int row_it = 0;
+		for (int i = 0; i < N; ++i) {
+			// skip rows of constant vertices
+			if (constant[i]) { continue; }
+
+			int col_it = 0;
+			glm::vec2 sums(0.0f,0.0f);
+			for (int j = 0; j < N; ++j) {
+				if (constant[j]) {
+					// if the vertex at the j-th column is constant
+					// we need to accumulate the sum
+					sums += ws[i][j]*uvs[j];
+				}
+				else {
+					// if it is not constant we have a
+					// triplet of the system matrix
+					if (ws[i][j] != 0.0f) {
+						triplet_list.push_back(T(row_it, col_it, ws[i][j]));
+					}
+					++col_it;
+				}
+			}
+
+			bX(row_it) = -sums.x;
+			bY(row_it) = -sums.y;
+			++row_it;
+		}
+
+		ws.clear();
+
+		#if defined (DEBUG)
+		cout << "    Building system matrix..." << endl;
+		#endif
+
+		/* build system matrix */
+		SparseMatrixf A(variable, variable);
+		A.setFromTriplets(triplet_list.begin(), triplet_list.end());
+		triplet_list.clear();
+		A.makeCompressed();
+
+		#if defined (DEBUG)
+		cout << "    Transpose of system matrix..." << endl;
+		#endif
+
+		SparseMatrixf At = A.transpose();
+
+		#if defined (DEBUG)
+		cout << "    Initialise solver..." << endl;
+		#endif
+
+		Eigen::SimplicialCholesky<SparseMatrixf> solver(At*A);
+
+		#if defined (DEBUG)
+		cout << "    Solving..." << endl;
+		cout << "        x" << endl;
+		#endif
+
+		Vectorf solX = solver.solve(At*bX);
+
+		#if defined (DEBUG)
+		cout << "        y" << endl;
+		#endif
+
+		Vectorf solY = solver.solve(At*bY);
+
+		int fixed_it = 0;
+		for (int i = 0; i < N; ++i) {
+			if (not constant[i]) {
+				uvs[i].x = solX(fixed_it);
+				uvs[i].y = solY(fixed_it);
+				++fixed_it;
+			}
+		}
 
 		return true;
 	}
