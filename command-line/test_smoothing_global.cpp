8106d61faa83c09234435c41d26b554794c8b891
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2018-12-03 12:09:01 +0100

    Removed 'parallel' function of global smoothing

--- command-line/test_smoothing_global.cpp
+++ command-line/test_smoothing_global.cpp
@@ -43,9 +43,6 @@ namespace test_geoproc {
 		cout << "                * uniform" << endl;
 		cout << "                * cotangent" << endl;*/
 		cout << endl;
-		cout << "    --threads n: number of threads to use." << endl;
-		cout << "        Default: 1" << endl;
-		cout << endl;
 	}
 
 	int test_smoothing_global(int argc, char *argv[]) {
@@ -54,7 +51,6 @@ namespace test_geoproc {
 		string mesh_file = "none";
 		string opt = "none";
 		string weight_type = "none";
-		size_t n_threads = 1;
 		float perc = 50.0f;
 
 		bool _print = false;
@@ -86,10 +82,6 @@ namespace test_geoproc {
 			}
 			else if (strcmp(argv[i], "--weight-type") == 0) {
 				weight_type = string(argv[i + 1]);
-				++i;
-			}
-			else if (strcmp(argv[i], "--threads") == 0) {
-				n_threads = atoi(argv[i + 1]);
 				++i;
 			}
 			else {
@@ -150,7 +142,6 @@ namespace test_geoproc {
 			return 1;
 		}
 		cout << " weights" << endl;
-		cout << "    using " << n_threads << " threads" << endl;
 		cout << "    percentage of fixed vertices: " << perc << "%" << endl;
 
 		TriangleMesh mesh;
@@ -184,7 +175,7 @@ namespace test_geoproc {
 		}
 
 		begin = timing::now();
-		smoothing::global::smooth(o, w, constant, n_threads, mesh);
+		smoothing::global::smooth(o, w, constant, mesh);
 		end = timing::now();
 
 		cout << "Smoothed mesh globally in "
--- geoproc/smoothing/global.cpp
+++ geoproc/smoothing/global.cpp
@@ -38,21 +38,6 @@ namespace global {
 	/* ---------------- */
 	/* HELPER FUNCTIONS */
 
-	static inline void compute_weights
-	(
-		int i, const smooth_weight& w,
-		const TriangleMesh& m, float *ws
-	)
-	{
-		if (w == smooth_weight::uniform) {
-			local_private::make_uniform_weights(i, m, ws);
-		}
-		else if (w == smooth_weight::cotangent) {
-			local_private::make_cotangent_weights(i, m, ws);
-		}
-		ws[i] = -1.0f;
-	}
-
 	static inline void compute_row
 	(
 		int i, int N, const smooth_weight& w, const vector<bool>& constant,
@@ -61,7 +46,18 @@ namespace global {
 	)
 	{
 		if (not constant[i]) {
-			compute_weights(i, w, m, ws);
+			// set to 0
+			for (int j = 0; j < N; ++j) {
+				ws[j] = 0.0f;
+			}
+
+			if (w == smooth_weight::uniform) {
+				local_private::make_uniform_weights(i, m, ws);
+			}
+			else if (w == smooth_weight::cotangent) {
+				local_private::make_cotangent_weights(i, m, ws);
+			}
+			ws[i] = -1.0f;
 		}
 		for (int j = 0; j < N; ++j) {
 			if (constant[j]) {
@@ -77,35 +73,6 @@ namespace global {
 				}
 				++col_it;
 			}
-		}
-	}
-
-	static inline void make_triplets
-	(
-		int a, int b, int N,
-		const vector<bool>& constant,
-		const smooth_weight& w,
-		const TriangleMesh& m,
-		float *ws,
-		vector<T>& triplet_list,
-		Vectorf& bX, Vectorf& bY, Vectorf& bZ
-	)
-	{
-		int row_it = 0;
-		for (int i = a; i < b; ++i) {
-			if (constant[i]) { continue; }
-
-			glm::vec3 sums(0.0f,0.0f,0.0f);
-			int col_it = 0;
-			compute_row
-			(i, N, w, constant, m, triplet_list, ws, row_it, col_it, sums);
-
-			// compute the right handside of the equation
-			// (vectors bX,bY,bZ) using this row
-			bX(row_it) = -sums.x;
-			bY(row_it) = -sums.y;
-			bZ(row_it) = -sums.z;
-			++row_it;
 		}
 	}
 
@@ -129,9 +96,25 @@ namespace global {
 		float *ws = static_cast<float *>(malloc(N*sizeof(float)));
 		// list of triplets for system's matrix
 		vector<T> triplet_list;
+
 		// build triplets for system's matrix
-		make_triplets
-		(0, N, N, constant, w, m, ws, triplet_list, bX, bY, bZ);
+		int row_it = 0;
+		for (int i = 0; i < N; ++i) {
+			if (constant[i]) { continue; }
+
+			glm::vec3 sums(0.0f,0.0f,0.0f);
+			int col_it = 0;
+			compute_row
+			(i, N, w, constant, m, triplet_list, ws, row_it, col_it, sums);
+
+			// compute the right handside of the equation
+			// (vectors bX,bY,bZ) using this row
+			bX(row_it) = -sums.x;
+			bY(row_it) = -sums.y;
+			bZ(row_it) = -sums.z;
+			++row_it;
+		}
+
 		// free memory
 		free(ws);
 
@@ -196,62 +179,6 @@ namespace global {
 		// this will never happen
 		return false;
 	}
-
-	/* ------------------ */
-	/* PARALLEL SMOOTHING */
-
-	static inline void smooth_laplacian_par
-	(
-		int N, int variable, const smooth_weight& w,
-		const vector<bool>& constant, int nt,
-		TriangleMesh& m
-	)
-	{
-		(void)nt;
-		smooth_laplacian(N, variable, w, constant, m);
-	}
-
-	bool smooth
-	(
-		const smooth_operator& op, const smooth_weight& w,
-		const vector<bool>& constant, size_t n_threads, TriangleMesh& m
-	)
-	{
-		if (n_threads == 1) {
-			return smooth(op, w, constant, m);
-		}
-
-		if (op == smooth_operator::BiLaplacian) {
-			cerr << "Error: global smoothing not implemented for operator"
-				 << endl << "    Bi-Laplacian" << endl;
-			return false;
-		}
-
-		if (op == smooth_operator::TaubinLM) {
-			cerr << "Error: invalid value of smoothing operator 'TaubinLM'"
-				 << endl << "    for a global smoothing operation" << endl;
-			return false;
-		}
-
-		const int N = m.n_vertices();
-		// amount of constant vertices
-		int n_constant = 0;
-		#pragma omp parallel for num_threads(n_threads) reduction(+:n_constant)
-		for (int i = 0; i < N; ++i) {
-			n_constant += constant[i];
-		}
-		// amount of variable vertices
-		const int variable = N - n_constant;
-
-		if (op == smooth_operator::Laplacian) {
-			smooth_laplacian_par(N, variable, w, constant, n_threads, m);
-			return true;
-		}
-
-		// this will never happen
-		return false;
-	}
-
 
 } // -- namespace global
 } // -- namespace smoothing
