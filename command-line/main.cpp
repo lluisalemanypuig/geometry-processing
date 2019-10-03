ec27fea9796c26edf85d2cbe7a059106bd966bdb
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2019-10-03 09:38:05 +0200

    Updated LICENSE

--- command-line/main.cpp
+++ command-line/main.cpp
@@ -1,3 +1,24 @@
+/*********************************************************************
+ * Geometry Processing Project
+ * Copyright (C) 2018-2019 Lluís Alemany Puig
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact: Lluis Alemany Puig (lluis.alemany.puig@gmail.com)
+ *
+ ********************************************************************/
+
 // C includes
 #include <string.h>
 
@@ -34,6 +55,11 @@ void command_line_usage() {
 }
 
 int main(int argc, char *argv[]) {
+	cout << "Geometry Processing Project  Copyright (C) 2018-2019 Lluís Alemany Puig" << endl;
+	cout << "This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'." << endl;
+	cout << "This is free software, and you are welcome to redistribute it" << endl;
+	cout << "under certain conditions; type `show c' for details." << endl;
+
 	if (argc == 1) {
 		command_line_usage();
 		return 1;
